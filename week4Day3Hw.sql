2cbb5edfdec0203a6a611bdc1be586861db6048c
qwertyNippon <orlandohenry100@gmail.com>
2023-06-08 18:29:20 -0400

    qwerty

--- week4Day3Hw.sql
+++ week4Day3Hw.sql
@@ -2,13 +2,29 @@ Questions
 
 -- 1. List all customers who live in Texas (use
 -- JOINs)
-select customer.first_name
+select customer.first_name, customer.last_name, customer.customer_state
 from customer
+Full join address
+on customer = address
+
 -- 2. Get all payments above $6.99 with the Customer's Full
 -- Name
 
 -- 3. Show all customers names who have made payments over $175(use
 -- subqueries)
+select customer.first_name, customer.last_name
+from customer
+where customer_id
+
+SELECT *
+FROM customer
+WHERE customer_id IN (
+	SELECT customer_id
+	FROM payment
+	GROUP BY customer_id
+	HAVING SUM(amount) > 175
+	ORDER BY SUM(amount) DESC
+);
 
 -- 4. List all customers that live in Nepal (use the city
 -- table)
